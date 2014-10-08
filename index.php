fb1a089985629020caf090621b8d9a6c77eca45c
James Pederson <james@jpederson.com>
2014-10-07 22:02:31 -0500

    Fixing and adding styles to the post listing.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Home/catch-all template
 get_header(); ?>
 
 
-	<div class="content-area">
+	<div class="blog">
 
 		<div class="content-narrow">
 		<?php
@@ -19,9 +19,9 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 			?>
 			<div class="entry-content">
-				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
 				<?php the_excerpt(); ?>
-			</h4>
+			</div>
 			<?php
 		endwhile;
 		?>
